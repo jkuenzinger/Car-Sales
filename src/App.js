4b60521de8d4fba3ff7837f59ea69ed04388ee7d
jkuenzinger <jkuenzinger228@gmail.com>
2020-03-18 07:31:01 -0700

    mvp met

--- src/App.js
+++ src/App.js
@@ -1,48 +1,60 @@
 import React from 'react';
-
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+// below i will be import connect and the actions from actions.index file
+import {connect} from 'react-redux';
+import {addFeature, removeFeature, addTotal} from './actions/index';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+
+// I am now going to import props instead of the () into my app funciton so it gets
+// the props im going to connect to it. 
+// I am also going to move the my initial state to my reducer I am going to create!
+
+const App = props => {
 
   const removeFeature = item => {
-    // dispatch an action here to remove an item
+    // dispatching actions below to remove a feature
+    props.removeFeature(item)
+    props.addTotal(-item.price)
   };
 
   const buyItem = item => {
-    // dipsatch an action here to add an item
+    // dipsatching actions below to buy and item
+    props.addFeature(item)
+    props.addTotal(item.price)
   };
-
+// in the return now that I have set up my store and passe the data that was held here in as props
+// I so i will be chaning all the state calls to props as the app is now recieving it as props. 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} additionalPrice={props.additionalPrice} />
+        <AddedFeatures car={props.car} addFeature={props.addFeature} removeFeature={removeFeature} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures buyItem= {buyItem} additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+// now I am going to map state to props so that i have the correct props for the above functions
+function mapStateToProps(state) {
+  return{
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+    additionalPrice: state.additionalPrice
+  }
+}
+
+// now instaead of exporting app normally. I will be exporting it with connet map state to props
+// and the actions I have brought in so that all the data is in a way synced together
+
+export default connect(mapStateToProps, {
+  addFeature,
+  removeFeature,
+  addTotal
+})(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+// below im importing my reducer my createStore and Provider to set a up redux store here
+import {reducer} from './reducer/index';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+// below im setting up my store and pluging in the reduce i imported
+const store = createStore(reducer)
+
+// I then wrap my app in the provider and and attach the store props to it
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
